3f000c1508bf4ff5a991f583a88c19adfa5bb538
Sadman Sakib <sadmansakibchayon@gmail.com>
2024-10-21 06:27:24 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Navber from './Componetes/Navber/Navber'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+  <div>
+    <Navber></Navber>
+    <h1>hello</h1>
+    <img src="https://images.pexels.com/photos/610293/pexels-photo-610293.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="photograper"/>
+  </div>
+
+  )
+}
+
+export default App
---
+++ src/Componetes/Navber/Navber.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navber = () => {
+  return (
+  
+   <div className="hello">
+    <h1>Hello world</h1>
+    <input type="text" placeholder='Enter your name' />
+   </div>
+   
+  )
+}
+
+export default Navber
